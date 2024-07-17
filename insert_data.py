e6870b0d4976f9cf25daf8f89f4642d96dedd213
Renatha Vieira <126165217+vieirasre@users.noreply.github.com>
2024-07-17 09:01:24 +0100

    Update insert_data.py
    Milvus.from_texts()

--- insert_data.py
+++ insert_data.py
@@ -60,7 +60,7 @@ def connect_watsonx():
     
     return model
 
-def embed_text(text):
+def embed_documents(text):
     model = connect_watsonx()
     embedding = model.generate_embeddings(text)
     return embedding
@@ -90,7 +90,7 @@ def index_documents(connection_info, filenames, urls, titles):
     split_texts = text_splitter.create_documents(texts, metadata)
     logger.info(f"Documents chunked. Sending to Milvus.")
     try:
-        index = Milvus.from_documents(documents=split_texts, embedding=embed_text, connection_args=connection_info, collection_name=INDEX_NAME)
+        index = Milvus.from_texts(documents=split_texts, embedding=embed_documents, connection_args=connection_info, collection_name=INDEX_NAME)
         return index
     except Exception as e:
         logger.error(f"Failed to index documents: {e}")
